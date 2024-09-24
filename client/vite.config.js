72b450d382ce0b719fbdb14ffb7039d646f1d561
Varun Nair <73441687+varun021@users.noreply.github.com>
2024-09-24 19:36:39 +0530

    Add files via upload

---
+++ client/vite.config.js
@@ -0,0 +1,23 @@
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import path from 'path';
+
+export default defineConfig({
+  plugins: [react()],
+  
+  
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src'),
+    },
+  },
+  server: { // Corrected placement for proxy
+    proxy: {
+      '/api': {
+        target: 'http://localhost:5000',
+        changeOrigin: true,
+        secure: false,
+      },
+    },
+  },
+});
